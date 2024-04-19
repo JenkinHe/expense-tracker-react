246f618053a05dad13a0cf7a560e8ecdf3821429
Jenkibear <jenkinwork123@gmail.com>
2024-04-19 11:08:37 +1000

    main page title and add expense button

---
+++ expense-tracker/src/components/main/index.jsx
@@ -0,0 +1,24 @@
+import { Button, Flex, Heading } from "@chakra-ui/react";
+
+
+
+
+export default function Main(){
+    return <Flex textAlign={'center'} flexDirection={'column'} pr={'5'} pl={'5'}>
+        <Flex alignItems={'center'} justifyContent={'space-between'} mt={'12'}>
+            <Heading color={'blue.400'} display={['none','block','block','block','block']}>
+                Expense Tracker
+            </Heading>
+            <Flex alignItems={'center'}>
+                <Button bg={'blue.300'} color={'black'} ml={'4'}>
+                    Add new Transaction
+                </Button>
+            </Flex>
+
+        </Flex>
+        <FLex>
+            
+        </FLex>
+
+    </Flex>
+}
